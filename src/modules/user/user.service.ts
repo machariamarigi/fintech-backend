fa0cad689b98fa4b4778b0ebea6fdd7216040215
machariamarigi <machariamarigi@gmail.com>
2020-08-31 15:10:04 +0300

    create users service with a create method for new users

---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, Inject } from "@nestjs/common";
+import * as jwt from 'jsonwebtoken';
+import { Users } from "./user.entity";
+import { IUser } from "./user.interface";
+import { jwtConfig } from "src/config/jwtConfig";
+
+import crypto = require('crypto');
+
+@Injectable()
+export class UsersService {
+    constructor(
+        @Inject('USERS_REPOSITORY') private usersRepisotory: typeof Users
+    ) {}
+
+    public async create(user: IUser): Promise<any> {
+        const exists = await Users.findOne({ where: { Email: user.Email } })
+    
+        if(exists) {
+            throw new Error('This email has already been used')
+        } else {
+            user.Salt = crypto.randomBytes(128).toString('base64')
+            user.Password = crypto.createHmac('sha256', user.Password + user.Salt).digest('hex')
+            const newUser: any = await this.usersRepisotory.create<Users>(user)
+            const jwtToken = jwt.sign(user, process.env.JWT_KEY, jwtConfig)
+            newUser.Token = jwtToken
+            return newUser
+        }
+    }
+}
