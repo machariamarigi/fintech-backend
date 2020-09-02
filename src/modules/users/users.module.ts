5c4c68a6c2ca1d05e69cc4776cd5f2c92e23c826
machariamarigi <machariamarigi@gmail.com>
2020-09-02 16:52:32 +0300

    import accounts module to users module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersProviders } from './users.provider';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { AccountsModule } from '../accounts/accounts.module';
 
 @Module({
+    imports: [AccountsModule],
     controllers: [UsersController],
     providers: [
         UsersProviders,
