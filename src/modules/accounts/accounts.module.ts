062839c171fc2ad87e870ffea287e90cb5633940
machariamarigi <machariamarigi@gmail.com>
2020-09-02 16:52:03 +0300

    add the accounts controller, provider & service to the module

--- src/modules/accounts/accounts.module.ts
+++ src/modules/accounts/accounts.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../database/database.module';
+import { AccountsController } from './accounts.controller';
+import { AccountsProviders } from './accounts.provider';
+import { AccountsService } from './accounts.service';
 
-@Module({})
+@Module({
+    imports: [DatabaseModule],
+    controllers: [AccountsController],
+    providers: [
+        AccountsProviders,
+        AccountsService
+    ],
+    exports: [
+        AccountsProviders,
+        AccountsService
+    ],
+})
 export class AccountsModule {}
